1f359e56f7c1a58bc6f4f721f116f3f55bb4a4a2
JM <daran.dasasner@gmail.com>
2015-04-22 15:00:48 +0200

    u mnie dziala

--- gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenCreatingANewGame.java
+++ gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenCreatingANewGame.java
@@ -33,15 +33,15 @@ public class WhenCreatingANewGame {
     public void whenTheUserCreatesTheFirstGenerationAnEmptyUniverseShouldBeAddedToTheSession() {
         HttpServletRequest request = mock(HttpServletRequest.class);
         ModelAndView homeView = controller.firstGeneration(5, 5, request);
-        assertThat(homeView.getModel().get("universe"), is(not(nullValue())));
+        assertThat(homeView.getModel().get("uxniverse"), is(not(nullValue())));
     }
 
     @Test
     public void whenTheUserCreatesTheFirstGenerationTheUniverseDimensionsShouldBeAddedToTheSession() {
         HttpServletRequest request = mock(HttpServletRequest.class);
         ModelAndView homeView = controller.firstGeneration(3, 5, request);
-        assertThat((Integer) homeView.getModel().get("rows"), is(3));
-        assertThat((Integer) homeView.getModel().get("columns"), is(5));
+        assertThat((Integer) homeView.getModel().get("rows"), is(2));
+        assertThat((Integer) homeView.getModel().get("columns"), is(4));
 
     }
 }	
