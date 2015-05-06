da0013098acf2ac7c8b55790974da3a470fb26c0
JM <daran.dasasner@gmail.com>
2015-05-06 15:41:33 +0200

    poprawa buildu cd

--- gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenSpawningANewGeneration.java
+++ gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenSpawningANewGeneration.java
@@ -27,7 +27,7 @@ public class WhenSpawningANewGeneration {
     public void theIndexUrlShouldDisplayTheHomePage() {
         HomePageController controller = new HomePageController();
         ModelAndView homeView = controller.index();
-        assertThat(homeView.getViewName(), is(not("home")));
+        assertThat(homeView.getViewName(), is(("home")));
     }
 }	
 
